397d0355639e1971b9a1cd53bbce2ff49c51ebd5
Markosus <mark2002david@hotmail.com>
2017-01-10 21:08:33 -0500

    added mobile metatag

--- resources/views/layouts/homepagelayout.blade.php
+++ resources/views/layouts/homepagelayout.blade.php
@@ -4,6 +4,7 @@
 	<title>
 		Liberated Living
 	</title>
+	<meta name="viewport" content="width=device-width, initial-scale=1">
 
 	
 	 <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/twitter-bootstrap/3.3.6/css/bootstrap.min.css" integrity="sha384-1q8mTJOASx8j1Au+a5WDVnPi2lkFfwwEAa8hDDdjZlpLegxhjVME1fgjWPGmkzs7" crossorigin="anonymous">
