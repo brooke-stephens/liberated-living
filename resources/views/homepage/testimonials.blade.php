bad907789da41e7eac8e34d1f503766ae687ae55
Markosus <mark2002david@hotmail.com>
2017-01-24 14:35:58 -0500

    working on testimonial fold

--- resources/views/homepage/testimonials.blade.php
+++ resources/views/homepage/testimonials.blade.php
@@ -1,15 +1,56 @@
 <section>
+
+<h1 class="text-center">Testimonials</h1>
+<h2 class="text-center">What our clients have to say.</h2>
 	
 <div id="testimonialsfold">
 	<div class="container">
 
-	<h1 class="text-center">Testimonials</h1>
+	
 		
+		<div class="row">
+			
+			<div class="col-md-12 testimonials-container" text-center>
+
+				<div class="testimonials-inner-container text-center">
+
+					<img class="testimonial-image" src="{{ URL::to('/images/homepage/testimonialsusan.png' )}}">
+
+					<div class="quotewrapper">
+						<hr class="testimonial-hr">
+						<div class="quote-top"><span>&ldquo;</span></div>
+						<hr class="testimonial-hr">
+					</div>
+
+					<div class="testimonial-content">
+						<p>Good morning Susan, Just a quick note to thank you for speaking at Mood Menders last night. You were great! You spoke with lots of energy and great information that kept the audience engaged well past your time limit.
+						</p>
+						<br>
+						<span class="author">Charlie Cino.
+						<br>
+						<span class="author-job">President, Mood Menders Support Group</span>
+					</div>
+
+					<div class="quotewrapper">
+						<hr class="testimonial-hr">
+						<div class="quote-bottom"><span>&rdquo;</span></div>
+						<hr class="testimonial-hr">
+					</div>
+					
+
+				</div>
+				
+
+			</div>
+
+		</div>
 
 
 
 	</div> <!-- end container -->
 </div> <!-- end testimonials -->
+
+<div class="triangle"></div>
 
 <div style="clear:both"></div>
 </section>
