5c4594df113f79db81916c666e22ff96a77c043d
orbithub <brooke.stephens@gmail.com>
2017-01-24 22:24:58 -0500

    susan updates

--- resources/views/homepage/services.blade.php
+++ resources/views/homepage/services.blade.php
@@ -18,31 +18,14 @@
 
 		    <div class="row row-centered">
 
-			      <div class="col-md-4 text-center">
 
-			      	<div class="leftservicebox">
-
-				      	<img class="serviceicon" src="{{ URL::to('/images/homepage/reikienergyicon.png') }}">
-				      	<h2>Reiki & Energy work</h2>
-				      	<p>Restore your energy balance!</p>
-				      	<!-- <p>Reiki restores energy balance and vitality by relieving the physical and emotional effects of unreleased stress. It accelerated healing, assists the body in cleansing toxins, and leaves one feeling relaxed and at peace.
-				      	</p> -->
-				      	<a href="" class="btn btn-primary">Learn More</a>
-
-
-			      	</div>
-
-
-
-			      </div>
-
-			      <div class="col-md-4 text-center">
+			      <div class="col-md-3 text-center">
 
 			      	<div class="rightservicebox">
 
 				      	<img class="serviceicon" src="{{ URL::to('/images/homepage/tonguediagnosisicon.svg') }}">
-				      	<h2>Tongue Diagnosis</h2>
-				      	<p>Stick out your tounge.</p>
+				      	<h2>Live Blood Analysis & Nutrition Consultation</h2>
+				      	<p><em>Sexy blood.  Sexy mind.</em></p>
 				      	<!-- <p>The tongue provides many visible indicators and interconnections with the body. These indicators can reveal your overall health and point to specific areas you need to improve upon.
 				      	</p> -->
 				      	<a href="" class="btn btn-primary">Learn More</a>
@@ -51,13 +34,13 @@
 
 			      </div>
 
-			      <div class="col-md-4 text-center">
+			      <div class="col-md-3 text-center">
 
 			      	<div class="leftservicebox">
 
 				      	<img class="serviceicon" src="{{ URL::to('/images/homepage/bloodanalysis.svg') }}">
-				      	<h2>Live Blood Analysis</h2>
-				      	<p>What's in my blood?</p>
+				      	<h2>Workshops</h2>
+				      	<p><em>Community Healing.</em></p>
 				      	<!-- <p>Taken from a fingertip prick, to 1,000 times or more under a microscope and can reveal certain subtleties missed by your doctor and laboratory blood tests.
 				      	</p> -->
 				      	<a href="{{ URL::to('/services/one-on-one/live-blood-analysis/') }}" class="btn btn-primary">Learn More</a>
@@ -68,62 +51,33 @@
 
 			      </div>
 
-			      <div class="col-md-4 text-center">
+			      <div class="col-md-3 text-center">
 
-			      	<div class="rightservicebox">
+					<div class="rightservicebox">
 
-				      	<img class="serviceicon" src="{{ URL::to('/images/homepage/wisdomcounseling.svg') }}">
-				      	<h2>Wisdom and Counseling</h2>
-				      	<p>Let's talk!</p>
-				      	<!-- <p>Feelings of low self-worth or abandonment? Stressed? Healing and transformation take place when we address both our negative and positive sides. Examine yourself and ego to diagnose pain and symtoms.
-				      	</p> -->
+						<img class="serviceicon" src="{{ URL::to('/images/homepage/wisdomcounseling.svg') }}">
+						<h2>Intuitive Counselling</h2>
+						<p><em>Soul meet Spirit.</em></p>
+						<!-- <p>Feelings of low self-worth or abandonment? Stressed? Healing and transformation take place when we address both our negative and positive sides. Examine yourself and ego to diagnose pain and symtoms.
+						</p> -->
 
-				      	<a href="" class="btn btn-primary">Learn More</a>
+						<a href="" class="btn btn-primary">Learn More</a>
 
-			      	</div>
-
+					</div>
 			      </div>
-
-			      <div class="col-md-4 text-center">
+			      <div class="col-md-3 text-center">
 
 			      	<div class="leftservicebox">
 
-				      	<img class="serviceicon" src="{{ URL::to('/images/homepage/nutritionconsultations.png') }}">
-				      	<h2>Nutrition Counseltations</h2>
-				      	<p>You are what you eat.</p>
-				      	<!-- <p>Including full health history, tongue diagnosis, pulse analysis, and symptomatology are all taken into account when determining the best food forward. Getting to the CORE of the imbalance so the body can heal itself is a must.
+				      	<img class="serviceicon" src="{{ URL::to('/images/homepage/reikienergyicon.png') }}">
+				      	<h2>Reiki & Energy <br>Work</h2>
+				      	<p><em>Get your Qi on!</em></p>
+				      	<!-- <p>Reiki restores energy balance and vitality by relieving the physical and emotional effects of unreleased stress. It accelerated healing, assists the body in cleansing toxins, and leaves one feeling relaxed and at peace.
 				      	</p> -->
-
 				      	<a href="" class="btn btn-primary">Learn More</a>
-
 			      	</div>
 			      </div>
-
-
-			      <div class="col-md-4 text-center">
-
-			      	<div class="leftservicebox">
-
-				      	<img class="serviceicon" src="{{ URL::to('/images/homepage/nutritionconsultations.png') }}">
-				      	<h2>Workshops</h2>
-				      	<p>Join the fun!</p>
-				      	<!-- <p>Including full health history, tongue diagnosis, pulse analysis, and symptomatology are all taken into account when determining the best food forward. Getting to the CORE of the imbalance so the body can heal itself is a must.
-				      	</p> -->
-
-				      	<a href="" class="btn btn-primary">Learn More</a>
-
-			      	</div>
-			      </div>
-
-			      <div class="col-md-4 text-center pull-right">
-
-
-			      </div>
-
-
 		    </div>	<!-- End row -->
-
-
 		</div>
 	</div>	<!-- end services -->
 
