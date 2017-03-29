5eeaf254ac1fbf6867135f3de4a934f8933073a3
Markosus <mark2002david@hotmail.com>
2017-03-29 12:51:25 -0400

    updated testimonials fold added user images and quotes

--- resources/views/homepage/testimonials.blade.php
+++ resources/views/homepage/testimonials.blade.php
@@ -6,6 +6,8 @@
     <li data-target="#myCarousel" data-slide-to="0" class="active"></li>
     <li data-target="#myCarousel" data-slide-to="1"></li>
     <li data-target="#myCarousel" data-slide-to="2"></li>
+    <li data-target="#myCarousel" data-slide-to="3"></li>
+    <li data-target="#myCarousel" data-slide-to="4"></li>
   </ol>
   <!-- Wrapper for slides -->
   <div class="carousel-inner" role="listbox">
@@ -18,9 +20,10 @@
 							<div class="testimonials-inner-container text-center">
 
 								<a id="my-testimonials"></a>
+
 								<h1 class="text-center">Testimonials</h1>	
 
-								<img class="testimonial-image" src="{{ URL::to('/images/homepage/testimonialsusan.png' )}}">
+								<img class="testimonial-image" src="{{ URL::to('/images/homepage/testimonialgwen.png' )}}">
 
 								<div class="quotewrapper">
 									<hr class="testimonial-hr">
@@ -29,12 +32,12 @@
 								</div>
 
 								<div class="testimonial-content">
-									<p>Good morning Susan, Just a quick note to thank you for speaking at Mood Menders last night. You were great! You spoke with lots of energy and great information that kept the audience engaged well past your time limit.
+									<p>Susan is not just a great nutritionist.  Her real gift is the ability to sense where a person's emotional blockage is in their body- and help them become aware of it, tune into it, work through it, and ultimately release it.  What could be more powerful than this?  We are often held back by patterns or behaviour that do not serve us, and that we may not fully understand.  Susan is able to get to the root of our issue- no matter what it is- and shed light on it.  I recommend her services to anyone who may be struggling with any issue in their life, and wanting support, or clarity. You will be so glad you did!"
 									</p>
 									<br>
-									<span class="author">Charlie Cino.
+									<span class="author">Gwen Nimeroski
 									<br>
-									<span class="author-job">President, Mood Menders Support Group</span>
+									<span class="author-job">Teacher</span>
 								</div>
 
 								<div class="quotewrapper">
@@ -62,7 +65,7 @@
 								<a id="my-testimonials"></a>
 								<h1 class="text-center">Testimonials</h1>	
 
-								<img class="testimonial-image" src="{{ URL::to('/images/homepage/testimonialsusan.png' )}}">
+								<img class="testimonial-image" src="{{ URL::to('/images/homepage/default.png' )}}">
 
 								<div class="quotewrapper">
 									<hr class="testimonial-hr">
@@ -71,10 +74,10 @@
 								</div>
 
 								<div class="testimonial-content">
-									<p>I just want to send you a quick email to say thanks for the session on Wednesday. I not only learned some valuable information from you but I also had a lot of fun with you too.
+									<p>Talking to you was what I needed.  More than anything else anyone had told me, your "FUCK IT" approach and everything else you were saying broke through a wall of emotion and helped me see things a little bit differently.  Your hug and humour and understanding gave me something to hold on to that I needed that day. You have an amazing gift for what you're doing - so many of us need it!
 									</p>
 									<br>
-									<span class="author">Emily Moon.
+									<span class="author">Kit Gagnon
 									<br>
 									<!-- <span class="author-job">President, Mood Menders Support Group</span> -->
 								</div>
@@ -91,7 +94,7 @@
 				</div> <!-- end container -->
 		</div> <!-- end testimonials -->
 		<div class="triangle" style="border-top: 70px solid #134b5b"></div>
-    </div>
+    </div><!-- end second item -->
 
     <div class="item">
      	   		<!-- start first item -->
@@ -104,7 +107,7 @@
 								<a id="my-testimonials"></a>
 								<h1 class="text-center">Testimonials</h1>	
 
-								<img class="testimonial-image" src="{{ URL::to('/images/homepage/testimonialsusan.png' )}}">
+								<img class="testimonial-image" src="{{ URL::to('/images/homepage/default.png' )}}">
 
 								<div class="quotewrapper">
 									<hr class="testimonial-hr">
@@ -113,10 +116,10 @@
 								</div>
 
 								<div class="testimonial-content">
-									<p>Hi Susan, I enjoyed the show today, but the best feature was seeing you talk. That was a real pleasure Susan, you are a fantastic speaker and so full of energy."
+									<p>I met Susan a few months ago and was introduced to a lady who gave one of the best presentations on nutrition, and  nutritional supplements that I've attended.    Her knowledge base was enormous, She gave out samples to illustrate her talk.  The green smoothies were excellent.   With humour and spontaneity she  was able to keep our large group interested beyond the 2 hour session.
 									</p>
 									<br>
-									<span class="author">Bob Snyder.
+									<span class="author">Elsie Kuly 
 									<br>
 									<!-- <span class="author-job">President, Mood Menders Support Group</span> -->
 								</div>
@@ -133,7 +136,89 @@
 				</div> <!-- end container -->
 		</div> <!-- end testimonials -->
 		<div class="triangle" style="border-top: 70px solid #4e2904"></div>
-    </div>
+    </div><!-- end third item -->
+      <div class="item">
+     	   		<!-- start first item -->
+     		<div id="testimonialsfold">
+				<div class="container">		
+					<div class="row">			
+						<div class="col-md-12 testimonials-container text-center">	
+							<div class="testimonials-inner-container text-center">
+
+								<a id="my-testimonials"></a>
+								<h1 class="text-center">Testimonials</h1>	
+
+								<img class="testimonial-image" src="{{ URL::to('/images/homepage/testimonialDyane.png' )}}">
+
+								<div class="quotewrapper">
+									<hr class="testimonial-hr">
+									<div class="quote-top"><span>&ldquo;</span></div>
+									<hr class="testimonial-hr">
+								</div>
+
+								<div class="testimonial-content">
+									<p>I cannot express my thanks enough for what Susan has done for me. A few days after I made my first appointment with Susan I discovered a lump in my breast. Susan was the second person I told, my doctor being the first. My first experience with Susan was one of comfort. She helped calm my fears and immediately went to work coming up with a plan to make me as healthy as possible before undergoing my cancer treatment. 
+									</p>
+									<br>
+									<span class="author">Dyane Cooper  
+									<br>
+									<!-- <span class="author-job">President, Mood Menders Support Group</span> -->
+								</div>
+
+								<div class="quotewrapper">
+									<hr class="testimonial-hr">
+									<div class="quote-bottom"><span>&rdquo;</span></div>
+									<hr class="testimonial-hr">
+								</div>					
+
+							</div>
+						</div>
+					</div>
+				</div> <!-- end container -->
+		</div> <!-- end testimonials -->
+		<div class="triangle"></div>
+    </div><!-- end 4th item -->
+        <div class="item">
+     	   		<!-- start first item -->
+     		<div id="testimonialsfold2">
+				<div class="container">		
+					<div class="row">			
+						<div class="col-md-12 testimonials-container text-center">	
+							<div class="testimonials-inner-container text-center">
+
+								<a id="my-testimonials"></a>
+								<h1 class="text-center">Testimonials</h1>	
+
+								<img class="testimonial-image" src="{{ URL::to('/images/homepage/default.png' )}}">
+
+								<div class="quotewrapper">
+									<hr class="testimonial-hr">
+									<div class="quote-top"><span>&ldquo;</span></div>
+									<hr class="testimonial-hr">
+								</div>
+
+								<div class="testimonial-content">
+									<p>Thankfully Susan helped me with talking to my inner child and releasing years and years of pent up anger and resentment. She has also helped move my dental phobia out to a point where I can go to the dentist office. This was never something I'd ever even considered before meeting Susan I would prefer to AVOID at all costs anything medical in nature. We have done blood work, altered my eating habits and even checked my hair for mineral defficiency's.  
+									</p>
+									<br>
+									<span class="author">Kim Moore 
+									<br>
+									<!-- <span class="author-job">President, Mood Menders Support Group</span> -->
+								</div>
+
+								<div class="quotewrapper">
+									<hr class="testimonial-hr">
+									<div class="quote-bottom"><span>&rdquo;</span></div>
+									<hr class="testimonial-hr">
+								</div>					
+
+							</div>
+						</div>
+					</div>
+				</div> <!-- end container -->
+		</div> <!-- end testimonials -->
+		<div class="triangle" style="border-top: 70px solid #134b5b"></div>
+    </div><!-- end 5th item -->
 
 
   </div>
