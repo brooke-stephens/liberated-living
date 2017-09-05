09fea5df3fb80ed22d546896fc724a8213ec0fbe
Markosus <mark2002david@hotmail.com>
2017-09-05 10:03:30 -0400

    Worked on testimonials user descriptions

--- resources/views/homepage/testimonialsupdate.blade.php
+++ resources/views/homepage/testimonialsupdate.blade.php
@@ -1,9 +1,10 @@
 <section>
 	
+	<div class="triangletop" style="border-bottom: 70px solid #798986"></div>
 	<div id="testimonialwrapper">
 		
 			<div class="container">
-
+			<a id="my-testimonials"></a>
 				<div class="row">
 
 					<div class="col-sm-6">
@@ -29,13 +30,13 @@
 
 					<div id="usertestimonial">
 
-							<img class="testi-image" src="{{ URL::to('/images/homepage/testimonialgwen.png' )}}">
-							<h1 class="testi-name">Gwen Nimeroski</h1>
-							<span class="author-job" style="font-size: 15px;">Teacher</span>
+							<img class="testi-image" src="{{ URL::to('/images/homepage/testimonialDyane.png' )}}">
+							<h1 class="testi-name">Dyane Cooper</h1>
+							<span class="author-job" style="font-size: 15px;">Client</span>
 							<br>
 							<img class="leaf-seperator" src="{{ URL::to('/images/homepage/leaf-seperator.svg' )}}">
 							<div class="testi-text-container2">
-								<p>Susan is not just a great nutritionist.  Her real gift is the ability to sense where a person's emotional blockage is in their body- and help them become aware of it, tune into it, work through it, and ultimately release it.  What could be more powerful than this?
+								<p>My first experience with Susan was one of comfort. She helped calm my fears and immediately went to work coming up with a plan to make me as healthy as possible before undergoing my cancer treatment.
 								</p>
 							</div>
 
@@ -52,13 +53,13 @@
 
 						<div id="usertestimonial">
 
-							<img class="testi-image" src="{{ URL::to('/images/homepage/testimonialgwen.png' )}}">
-							<h1 class="testi-name">Gwen Nimeroski</h1>
-							<span class="author-job" style="font-size: 15px;">Teacher</span>
+							<img class="testi-image" src="{{ URL::to('/images/homepage/default.png' )}}">
+							<h1 class="testi-name">Kit Gagnon</h1>
+							<span class="author-job" style="font-size: 15px;">Client</span>
 							<br>
 							<img class="leaf-seperator" src="{{ URL::to('/images/homepage/leaf-seperator.svg' )}}">
 							<div class="testi-text-container3">
-								<p>Susan is not just a great nutritionist.  Her real gift is the ability to sense where a person's emotional blockage is in their body- and help them become aware of it, tune into it, work through it, and ultimately release it.  What could be more powerful than this?
+								<p>Talking to you was what I needed. Your hug and humour and understanding gave me something to hold on to that I needed that day. You have an amazing gift for what you're doing - so many of us need it!
 								</p>
 							</div>
 
@@ -71,13 +72,13 @@
 
 					<div id="usertestimonial">
 
-							<img class="testi-image" src="{{ URL::to('/images/homepage/testimonialgwen.png' )}}">
-							<h1 class="testi-name">Gwen Nimeroski</h1>
-							<span class="author-job" style="font-size: 15px;">Teacher</span>
+							<img class="testi-image" src="{{ URL::to('/images/homepage/default.png' )}}">
+							<h1 class="testi-name">Elsie Kuly </h1>
+							<span class="author-job" style="font-size: 15px;">Client</span>
 							<br>
 							<img class="leaf-seperator" src="{{ URL::to('/images/homepage/leaf-seperator.svg' )}}">
 							<div class="testi-text-container4">
-								<p>Susan is not just a great nutritionist.  Her real gift is the ability to sense where a person's emotional blockage is in their body- and help them become aware of it, tune into it, work through it, and ultimately release it.  What could be more powerful than this?
+								<p>I met Susan a few months ago and was introduced to a lady who gave one of the best presentations on nutrition, and  nutritional supplements that I've attended.  Her knowledge base was enormous, She gave out samples to illustrate her talk.  The green smoothies were excellent.
 								</p>
 							</div>
 
@@ -92,7 +93,9 @@
 
 
 
+
 	</div> <!-- endtestimonialwrapper -->
+	<div class="triangle" style="border-top: 70px solid #798986"></div>
 
 
 <div style="clear:both"></div>
